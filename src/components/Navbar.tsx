279de4f32990c97a1bb2487d0050a78ff3bcee42
AlexGthr <agauthier5@outlook.fr>
2024-10-05 13:56:46 +0200

    Ajout de l'authentification sur le projet

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,14 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
 import { ArrowRight } from 'lucide-react'
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-const Navbar = () => {
-
-    const user = undefined
-    const isAdmin = false
+const Navbar = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+    
+    const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
@@ -50,19 +53,19 @@ const Navbar = () => {
 
                     // Si l'utilisateur n'est pas log, on affiche cette partie de la navbar
                         <>
-                        <Link href="/api/auth/register" className={buttonVariants({
+                        <RegisterLink className={buttonVariants({
                             size: 'sm',
                             variant: 'ghost',
                         })}>
                             Sign up
-                        </Link>
+                        </RegisterLink>
 
-                        <Link href="/api/auth/login" className={buttonVariants({
+                        <LoginLink className={buttonVariants({
                             size: 'sm',
                             variant: 'ghost',
                         })}>
                             Login
-                        </Link>
+                        </LoginLink>
 
                         <div className='h-8 w-px bg-zinc-200 hidden sm:block'/>
 
